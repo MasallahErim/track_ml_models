1906bfc41161fda49c95e663b2988ad427b3be40
Toynet8 <masallaherim@gmail.com>
2023-05-26 21:38:56 +0300

    setup dvc piplines

--- src/models/load_data.py
+++
@@ -1,8 +0,0 @@
-
-import opendatasets as od
-import pandas as pd
-
-url2 = "https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/383116/rawdata_new.csv"
-od.download(url2)
-
-print("data installed successfully")
